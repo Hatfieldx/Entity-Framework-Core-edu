4b2c10215bcb86e0d1831d49875d07b960990741
Hatfieldx <tsyganov.nn@gmail.com>
2020-05-12 15:25:03 +0700

    added hw lesson 2

---
+++ lesson 2 - DDL Annotation & Fluent api/EFCoreDDLApp/custom_logger/AppContext.cs
@@ -0,0 +1,32 @@
+﻿using custom_logger.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace custom_logger
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=TestDB_001;Trusted_Connection=True;");
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Tablet>();
+        //}
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Ignore<Company>();
+        //}
+    }
+}
---
+++ lesson 2 - DDL Annotation & Fluent api/EFCoreDDLApp/custom_logger/Entities/Company.cs
@@ -0,0 +1,10 @@
+﻿
+namespace custom_logger.Entities
+{
+    //[NotMapped]
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
